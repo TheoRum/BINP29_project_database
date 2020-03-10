f225c992ba873a776480045cfdf357401838e57b
Theodor Rumetshofer <theo@theodors-mbp.home>
2020-03-10 16:01:20 +0100

    create python file

---
+++ src/flask_sle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+
+ | Title:
+     flask_sle.py
+ | Date:
+     2020-03-09
+ | Author(s):
+     THEODOR Rumetshofer
+ | Description:    
+     ...
+     
+ | List of functions:
+     none
+
+ | List of "non standard" modules:
+     none
+
+ | Procedure:
+     1) ...
+
+ | Usage:
+     python flask_sle.py 
+
+"""
+
+ 
+from flask import Flask
+
+#_____________
+# define file \_______________________________________________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
